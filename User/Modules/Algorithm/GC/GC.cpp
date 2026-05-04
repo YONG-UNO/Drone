0946b6e3ed020cc39516e5f6bb233c58bee3c23f
dingyong <2903964879@qq.com>
2026-05-04 17:42:00 +0800

    添加重力补偿

---
+++ User/Modules/Algorithm/GC/GC.cpp
@@ -0,0 +1,5 @@
+//
+// Created by DingYong on 2026/5/4.
+//
+
+#include "GC.hpp"
---
+++ User/Modules/Algorithm/GC/GC.hpp
@@ -0,0 +1,8 @@
+//
+// Created by DingYong on 2026/5/4.
+//
+
+#ifndef DRONE_GC_HPP
+#define DRONE_GC_HPP
+
+#endif //DRONE_GC_HPP
